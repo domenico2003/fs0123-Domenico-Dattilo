4c35ad0461727b0a175d2c9dd5f1699239b97909
domenico2003 <domdat03@gmail.com>
2023-02-08 18:10:16 +0100

    aggiornamento-homework

--- m1/s2/g3/homework/js/D3.js
+++ m1/s2/g3/homework/js/D3.js
@@ -244,6 +244,8 @@ console.log(me)
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
+/* SCRIVI QUI LA TUA RISPOSTA */
+
 let array1 = []
 
 for (u =1; u <= 10; u++){
@@ -252,7 +254,7 @@ for (u =1; u <= 10; u++){
 
 console.table(array1)
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.

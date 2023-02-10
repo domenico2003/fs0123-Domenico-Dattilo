6691fb637dbaf7d0ce052be4032841c50e82d842
domenico2003 <domdat03@gmail.com>
2023-02-10 15:03:56 +0100

    compito senza extra

---
+++ m1/s2/progetto settimanale/extra/assets/js/script.js
@@ -0,0 +1,22 @@
+//per aggiungere numeri al display
+function display(elemento){
+    let tasto= elemento.getAttribute('data-tasto')
+    let display = document.querySelector('#display')
+    
+    display.value += tasto
+}
+
+
+//per far resettare la calcolatrice
+function cancella(){
+    let display = document.querySelector('#display')
+    display.value = ""
+}
+
+//per effettuare il calcolo
+function calcolo() {
+    let display = document.querySelector('#display')
+
+      
+   
+}

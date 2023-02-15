b0afb7b45ef6dcf262ac6e20f1e8020ce8bab0be
domenico2003 <domdat03@gmail.com>
2023-02-15 18:08:22 +0100

    homework
    Co-authored-by: AlfonsoDeBartolo <AlfonsoDeBartolo@users.noreply.github.com>
    Co-authored-by: andreadipaola <andreadipaola@users.noreply.github.com>
    Co-authored-by: Edmondo Adamo <Elmundo29@users.noreply.github.com>
    Co-authored-by: Giuseppe Canzoneri <GiuseppeCanzoneri@users.noreply.github.com>

---
+++ m1/s3/g3/homework/assets/js/script.js
@@ -0,0 +1,28 @@
+let target= document.getElementById("target");
+let input = document.getElementById("testo");
+
+
+
+function test() {
+
+    let element = document.createElement("div");
+    let bottone= document.createElement('button');
+    bottone.setAttribute(onchange,eliminazione())
+    let li =document.createElement('li')
+    element.classList.add('task')
+    bottone.classList.add('bottone')
+    li.classList.add('li')
+
+if(input.value  !==  ""){
+    li.appendChild(document.createTextNode(input.value));
+    document.getElementById('target').appendChild(element);
+    element.appendChild(li)
+   element.appendChild(bottone)
+   bottone.appendChild(document.createTextNode('DELETE'));
+    bottone.addEventListener('click',()=>{
+        element.remove() })
+    
+    }
+  
+}
+ 

1247c13470b930a4c6a99ac25d113bd6709676e2
domenico2003 <domdat03@gmail.com>
2023-02-16 00:55:43 +0100

    completamento homework

--- m1/s3/g3/homework/assets/js/script.js
+++ m1/s3/g3/homework/assets/js/script.js
@@ -6,22 +6,29 @@ let input = document.getElementById("testo");
 function test() {
 
     let element = document.createElement("div");
-    let bottone= document.createElement('button');
-    let li =document.createElement('li')
     element.classList.add('task')
-    bottone.classList.add('bottone')
-    li.classList.add('li')
 
+    let bottone= document.createElement('button');
+    bottone.classList.add('bottone')
+
+    let li =document.createElement('li')
+    li.classList.add('li')
+    
     if(input.value  !==  ""){
         li.appendChild(document.createTextNode(input.value));
         document.getElementById('target').appendChild(element);
         element.appendChild(li)
         element.appendChild(bottone)
-        bottone.appendChild(document.createTextNode('DELETE'));
-        bottone.addEventListener('click',()=>{
-            element.remove() })
-    
+        bottone.appendChild(document.createTextNode('DELETE'));    
     }
-  
+
+    bottone.addEventListener('click',()=>{element.remove() })
+    
+   
+    if(li.style.textDecoration !== 'line-through'){
+        li.addEventListener('click',()=>{li.style.textDecoration= 'line-through'})
+    }
+    
+    input.value = ""
 }
  
